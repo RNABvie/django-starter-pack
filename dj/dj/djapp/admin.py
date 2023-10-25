560e1739d2ae2db172bbe94e46cf12a7d3f7f78e
Rin <rnabieeev@gmail.com>
2023-10-25 17:40:51 +0300

    directory_clean

--- dj/dj/djapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from djapp import models
-# Register your models here.
-
-admin.site.register(models.Worker)
-admin.site.register(models.News)
-admin.site.register(models.Rating)
-admin.site.register(models.UserProfile)
--- dj/dj/djapp/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from djapp import models
-
-
-class WorkerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Worker
-        fields = '__all__'
